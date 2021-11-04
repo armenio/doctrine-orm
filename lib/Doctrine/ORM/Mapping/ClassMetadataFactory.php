1c17fa230ff41816438404b6207087a2b82cc9b9
Rafael Armenio <rafael.armenio@gmail.com>
2021-11-04 16:15:40 -0300

    Revert "Merge pull request #8916 from greg0ire/failing-test-8914"
    This reverts commit 77cc86ed880e3f1f6c9c5819e131a8aaeeeee0da, reversing
    changes made to 1de28c2cab5cd7e3c9d781a8beff22e328190911.

--- lib/Doctrine/ORM/Mapping/ClassMetadataFactory.php
+++ lib/Doctrine/ORM/Mapping/ClassMetadataFactory.php
@@ -285,10 +285,7 @@ class ClassMetadataFactory extends AbstractClassMetadataFactory
                 }
             } else {
                 assert($parent instanceof ClassMetadataInfo); // https://github.com/doctrine/orm/issues/8746
-                if (
-                    (! $class->reflClass || ! $class->reflClass->isAbstract())
-                    && ! in_array($class->name, $class->discriminatorMap)
-                ) {
+                if ((! $class->reflClass || ! $class->reflClass->isAbstract()) && ! in_array($class->name, $parent->discriminatorMap)) {
                     throw MappingException::mappedClassNotPartOfDiscriminatorMap($class->name, $class->rootEntityName);
                 }
             }
